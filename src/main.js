6b54dab0d8d4e8d2bb71f48ba44f5f3165473cdc
Yohan <yohan@yohanpro.com>
2019-11-18 09:43:25 +0900

    copy file test

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import {
-  promisify
+	promisify
 } from 'util';
 
 const access = promisify(fs.access);
@@ -43,13 +43,30 @@ const copy = promisify(ncp);
 //   return true;
 // }
 
+async function copyTemplateFiles(options) {
+	return copy(options.templateDirectory, options.targetDirectory);
+}
 export async function createProject(options) {
-  console.log(options)
-  const currentUrl = `${process.cwd()}/${options.template}`;
-  console.log(currentUrl)
-  try {
-    fs.mkdirSync(currentUrl)
-  } catch (err) {
-    console.log(err);
-  }
+	options = {
+		...options,
+		targetDirectory: options.targetDirectory || process.cwd()
+	};
+	const currentUrl = `${process.cwd()}/${options.template}`;
+	const currentFileUrl =
+		import.meta.url; // file:///Users/gim-yohan/Projects/Veeva_CLM-boilerplate_3.0/src/main.js
+	const templateDir = path.resolve(
+		new URL(currentFileUrl).pathname, // /Users/gim-yohan/Projects/Veeva_CLM-boilerplate_3.0/src/main.js
+		'../../templates'
+	);
+	options.templateDirectory = templateDir;
+	try {
+		fs.mkdirSync(currentUrl);
+	} catch (err) {
+		if (err.code === 'EEXIST') {
+			console.log('%s Project is already exist or Duplicate', chalk.red.bold('ERROR'));
+			process.exit(1);
+		}
+	}
+	await copyTemplateFiles(options);
+	console.log('%s Project ready', chalk.green.bold('DONE'));
 }
