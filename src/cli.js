b4b80e34be47b82b2d9e9dbd1f9e10a393dba557
imac.yohan <yohan.kim@innoboost.co.kr>
2019-11-19 14:38:45 +0900

    템플릿 만들고 복사 테스트

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,17 @@
 import arg from "arg";
 import inquirer from "inquirer";
-import { createProject } from "./main";
+import {
+  createProject
+} from "./main";
+
 function parseArgumentsIntoOptions(rawArgs) {
-  const args = arg(
-    {
-      "--git": Boolean,
-      "--install": Boolean,
-      "-i": "--install"
-    },
-    {
-      argv: rawArgs.slice(2)
-    }
-  );
+  const args = arg({
+    "--git": Boolean,
+    "--install": Boolean,
+    "-i": "--install"
+  }, {
+    argv: rawArgs.slice(2)
+  });
   return {
     git: args["--git"] || false,
     template: args._[0],
@@ -19,17 +19,13 @@ function parseArgumentsIntoOptions(rawArgs) {
   };
 }
 async function promptForMissingOptions(options) {
-  const defaultTemplate = "JavaScript";
   const questions = [];
-  if (!options.template) {
-    questions.push({
-      type: "list",
-      name: "template",
-      message: "Please choose which project template to use",
-      choices: ["JavaScript", "TypeScript"],
-      default: defaultTemplate
-    });
-  }
+
+  questions.push({
+    type: "input",
+    name: "presentation",
+    message: "Name of Veeva root Presentation"
+  });
   if (!options.git) {
     questions.push({
       type: "confirm",
@@ -42,9 +38,14 @@ async function promptForMissingOptions(options) {
     type: "number",
     name: "slide",
     message: "Total number of Slide?",
-    validate: function(value) {
+    validate: function (value) {
       var valid = !isNaN(parseFloat(value));
-      return valid || "Please enter a number";
+      if (!valid) {
+        console.log("Please enter number")
+        process.exit(1);
+      } else {
+        return true
+      }
     }
   });
   questions.push({
@@ -55,15 +56,17 @@ async function promptForMissingOptions(options) {
   questions.push({
     type: "confirm",
     name: "seperate",
-    message: "Do you want Seperate Main and Add?",
+    message: "Do you want to seperate Main and Add?",
     default: false
   });
+
   const answers = await inquirer.prompt(questions);
 
   return {
     ...options,
     git: options.git || answers.git,
     template: options.template || answers.template,
+    presentation: options.presentation || answers.presentation,
     ...answers
   };
 }
